500986a0814f9f8a7166f6381925390c70485733
pxrty <kpathana3495@gmail.com>
2023-11-15 17:01:48 -0600

    deez

--- lightclientnew.py
+++ lightclientnew.py
@@ -1,80 +1,55 @@
-# lightclient.py
-# from EmulatorGUI import GPIO
 import socket
+import sys
 import struct
 import RPi.GPIO as GPIO
 import time
-import sys
 
 PIR = 24
 GPIO.setmode(GPIO.BCM)
 GPIO.setup(PIR, GPIO.IN)
 
 def create_packet(s_n, ack_n, ack, syn, fin, payload):
-    header = struct.pack('!II', s_n, ack_n)
-    flags = (ack << 2) | (syn << 1) | fin
-    header += struct.pack('!B', flags)
-    data = header + payload.encode()
-    return data
+    data = struct.pack('!I', s_n)  # pack the sequence number
+    data += struct.pack('!I', ack_n)  # pack the acknowledgement number
+    data += struct.pack("!c", ack.encode())  # pack the ACK
+    data += struct.pack("!c", syn.encode())  # pack the SYN
+    data += struct.pack("!c", fin.encode())  # pack the FIN
+    data += payload.encode()  # pack the payload
+    return data  
 
 def main():
-    
-    #if len(sys.argv) != 7 or sys.argv[1] != "-s" or sys.argv[3] != "-p" or sys.argv[5] != "-l":
-     #   print("Usage: lightclient -s <SERVER-IP> -p <PORT> -l <LOGFILE>")
-      #  sys.exit(1)
-    
-    # Parse command line arguments
-    server_ip = '127.0.0.1'
-    port = 1337
-    log_file = '/home/pi/Desktop/lab4200/4200/server.log'
-
-    sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-
-    server_address = (server_ip, port)
-
     try:
-        # Three-way handshake
-        seq_num = 100
-        ack_num = 0
-        message = create_packet(seq_num, ack_num, 0, 1, 0, '')
-        sock.sendto(message, server_address)
+        # Parse command line arguments
+        server_ip = '127.0.0.1'
+        port = 1337
+        log_file = '/home/pi/Desktop/lab4200/4200/server.log'
 
-        data, server = sock.recvfrom(4096)
-        s_n, ack_n, flags, payload = struct.unpack('!IIB' + str(len(data)-9) + 's', data)
-        if flags & 0b010:
-            print("Handshake completed.")
-            seq_num += 1
-            ack_num = ack_n + 1
+        # Create a UDP socket
+        sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 
-        message = create_packet(seq_num, ack_num, 1, 0, 0, 'DurationAndBlinks')
-        sock.sendto(message, server_address)
+        server_address = (server_ip, port)
 
-        data, server = sock.recvfrom(4096)
-        s_n, ack_n, flags, payload = struct.unpack('!IIB' + str(len(data)-9) + 's', data)
-        if flags & 0b001:
-            print(f"Server acknowledged: {payload.decode()}")
-
-        # Main loop
         while True:
-            if GPIO.input(PIR):
-                message = create_packet(seq_num, ack_num, 1, 0, 0, 'MotionDetected')
-                sock.sendto(message, server_address)
+            try:
+                # Detect motion
+                if GPIO.input(PIR):
+                    print("Motion detected!")
+                    # Motion detected, send a packet to the server
+                    message = create_packet(100, 0, 'Y', 'N', 'N', 'MotionDetected')
+                    print("Sending 'MotionDetected' message to server...")
+                    sock.sendto(message, server_address)
+                    time.sleep(3)
+                else:
+                    # No motion detected, send 'Hello, server' message
+                    message = create_packet(100, 0, 'Y', 'N', 'N', 'Hello, server')
+                    print("Sending 'Hello, server' message to server...")
+                    sock.sendto(message, server_address)
+                    time.sleep(2)
 
-                data, server = sock.recvfrom(4096)
-                s_n, ack_n, flags, payload = struct.unpack('!IIB' + str(len(data)-9) + 's', data)
-                if flags & 0b001:
-                    print(f"Server acknowledged motion detection.")
-
-            # Your other conditions and actions can be added here
-
-            time.sleep(1)  # Adjust the sleep duration as needed
-
-    except KeyboardInterrupt:
-        print("Keyboard interrupt. Exiting.")
+            except KeyboardInterrupt:
+                print("Closing...")
+                break
     finally:
-        # Send FIN packet
-        message = create_packet(seq_num, ack_num, 0, 0, 1, 'InteractionCompleted')
-        sock.sendto(message, server_address)
         sock.close()
 
 if __name__ == "__main__":
